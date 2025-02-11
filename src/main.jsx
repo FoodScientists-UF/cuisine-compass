cdb0f7791c97ee341f94ff0ca06030e10c4556e3
d4julian <julianubico@gmail.com>
2025-02-11 15:51:09 -0500

    Added user page to see user's information

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Auth from "./Auth.jsx";
 import "./index.css";
 import App from "./App.jsx";
-import Explore from "./Explore.jsx";
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import Onboarding from "./pages/Onboarding.jsx";
+import User from "./pages/User.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/explore" element={<Explore />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/user" element={<User />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
