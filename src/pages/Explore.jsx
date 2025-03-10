f9e55a5a3f7ee2f442677d825cb9e195eed0284a
julachancey <julia.chancey@ufl.edu>
2025-03-10 14:57:14 -0400

    hover feature closer to figma wireframe

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -9,11 +9,11 @@ import img5 from "../layouts/images/Img5.jpg";
 import "./Explore.css";
 
 const recipes = [
-  { id: 1, title: "Spaghetti Carbonara", image: img1, cost: "$$", likes: 25, time: "30 minutes" },
-  { id: 2, title: "Avocado Toast", image: img2, cost: "$", likes: 40, time: "10 minutes" },
-  { id: 3, title: "Sushi Rolls", image: img3, cost: "$$$", likes: 15, time: "50 minutes" },
-  { id: 4, title: "Grilled Steak", image: img4, cost: "$$", likes: 35, time: "45 minutes" },
-  { id: 5, title: "Smoothie Bowl", image: img5, cost: "$", likes: 50, time: "15 minutes" },
+  { id: 1, title: "Spaghetti Carbonara", chef: "@ju_chancey", image: img1, cost: "$$", likes: 25, time: "30 minutes", tags: [] },
+  { id: 2, title: "Avocado Toast", chef: "@sydaurelius", image: img2, cost: "$", likes: 40, time: "10 minutes", tags: ["Vegetarian"] },
+  { id: 3, title: "Sushi Rolls", chef: "@julian.ubico", image: img3, cost: "$$$", likes: 15, time: "50 minutes", tags: [] },
+  { id: 4, title: "Grilled Steak", chef: "@alexa.gonz", image: img4, cost: "$$", likes: 35, time: "45 minutes", tags: [] },
+  { id: 5, title: "Smoothie Bowl", chef: "@cam_cimber", image: img5, cost: "$", likes: 50, time: "15 minutes", tags: ["Gluten-Free"] },
 ];
 
 const ExplorePage = () => {
@@ -27,9 +27,17 @@ const ExplorePage = () => {
               <div className="overlay">
                 <div className="recipe-info">
                   <h3>{recipe.title}</h3>
+                  <p>{recipe.chef}</p>
+                  {recipe.tags.length > 0 && (
+                    <div className="tags">
+                      {recipe.tags.map((tag, index) => (
+                        <span key={index} className="tag">{tag}</span>
+                      ))}
+                    </div>
+                  )}
                   <p>{recipe.cost}</p>
                   <p>❤ {recipe.likes}</p>
-                  <p>{recipe.time}</p>
+                  <p>🕒 {recipe.time}</p>
                 </div>
               </div>
             </div>
