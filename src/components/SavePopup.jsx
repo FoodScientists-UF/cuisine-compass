dc3d2111410a9543fa915070c33a67140ed3c2b0
Sydney Aurelius <sydneyaurelius@yahoo.com>
2025-03-11 20:21:18 -0400

    Make SavePopup closer to Figma

--- src/components/SavePopup.jsx
+++ src/components/SavePopup.jsx
@@ -7,14 +7,14 @@ export default function SavePopup({ collections, savedCollections, callback }) {
     return savedCollections.map((c) => c.folder_id).includes(collection.id);
   };
   return (
-    <div className="absolute mt-2 right-0 w-96 rounded-lg shadow-lg border border-gray-200 z-10">
+    <div className="absolute mt-2 right-0 w-96 rounded-lg shadow-lg border border-gray-200 z-10 bg-white">
       <div className="p-4 flex flex-col gap-y-4">
         <span className="text-xl abhaya-libre-semibold text-center">Save</span>
         <div className="flex flex-col gap-y-4">
           {collections.map((collection, index) => (
             <div
               key={index}
-              className="flex flex-row justify-between items-center"
+              className="flex flex-row justify-between items-center px-3 py-2 rounded-md hover:bg-[#F3F3F3] transition"
             >
               <div className="flex flex-row gap-x-5 items-center">
                 <img
@@ -25,22 +25,24 @@ export default function SavePopup({ collections, savedCollections, callback }) {
                   {collection.name}
                 </span>
               </div>
-              <Checkbox
-                checked={isCollectionSaved(collection)}
-                onChange={() =>
-                  callback(collection, isCollectionSaved(collection))
-                }
-              />
+              <button
+                className={`${
+                  isCollectionSaved(collection) ? "bg-gray-500" : "bg-[#D75600]"
+                } flex flex-row items-center gap-x-2 text-white px-5 py-1 rounded-full text-lg abhaya-libre-semibold`}
+                onClick={() => callback(collection, isCollectionSaved(collection))}
+              >
+                {isCollectionSaved(collection) ? "Saved!" : "Save"}
+              </button>
             </div>
           ))}
         </div>
         <div className="bg-gray-400 h-[0.5px]"></div>
-        <div className="flex flex-row items-center gap-x-5">
+        <div className="flex flex-row items-center gap-x-5 px-3 py-2 rounded-md hover:bg-[#F3F3F3] transition">
           <div className="relative w-12 h-12"> 
             <img className="w-12 h-12 rounded-md bg-gray-300" />
             <FiPlus className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-black w-5 h-5"/>
           </div>
-          <button className="abhaya-libre-regular text-xl hover:text-orange-500 transition">
+          <button className="abhaya-libre-regular text-xl">
             Create a collection
           </button>
         </div>
